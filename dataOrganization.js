18167868632249600ffd63c5277d29c75f1e08c1
Vineet <100793773+Mr-Vineet@users.noreply.github.com>
2024-12-23 17:01:13 +0530

    arranged data and wrote function for Q1

---
+++ dataOrganization.js
@@ -0,0 +1,113 @@
+const peopleDetails = [
+  {
+    name: "Rahul",
+    designation: "software engineer",
+    studiedCS: true,
+    place: "Pune",
+    currentlyEmployed: true,
+    ownsCar: true,
+    hobbies: [{ type: "playing chess" }, { type: "gardening" }],
+    petsInfo: [
+      {
+        petName: "Max",
+        petType: "golden retriever",
+        age: 4,
+        isVaccinated: true,
+        favoriteActivity: "fetch in the park",
+      },
+    ],
+  },
+  {
+    name: "Ananya",
+    age: 30,
+    studiedCS: true,
+    place: "Bangalore",
+    currentlyEmployed: false,
+    ownsCar: false,
+    hobbies: [
+      {
+        type: "cooking",
+      },
+    ],
+    petsInfo: [
+      {
+        petName: "Kiwi",
+        petType: "parrot",
+        isVaccinated: true,
+      },
+    ],
+  },
+  {
+    name: "Ramesh",
+    age: 45,
+    designation: "business owner",
+    studiedCS: false,
+    place: "Jaipur",
+    currentlyEmployed: true,
+    ownsCar: false,
+    hobbies: [
+      {
+        type: "gardening",
+      },
+      {
+        type: "reading",
+        genre: "historical fiction",
+      },
+    ],
+    petsInfo: [
+      {
+        petName: "Bella",
+        petType: "persian cat",
+        age: 3,
+        isVaccinated: true,
+        favoriteActivity: "lounging in the sun",
+      },
+      {
+        petName: "Leo",
+        petType: "persian cat",
+        age: 3,
+        isVaccinated: true,
+        favoriteActivity: "lounging in the sun",
+      },
+    ],
+  },
+  {
+    name: "Kavya",
+    age: 28,
+    designation: "dancer",
+    studiedCS: false,
+    place: "Chennai",
+    currentlyEmployed: false,
+    ownsCar: false,
+    hobbies: [
+      {
+        type: "binge-watching sci-fi shows",
+      },
+      {
+        type: "reading",
+        genre: "modern fantasy novels",
+      },
+    ],
+    petsInfo: [
+      {
+        petName: "Snowy",
+        petType: "rescue rabbit",
+        age: 2,
+        isVaccinated: true,
+        favoriteActivity:
+          "enjoys hopping around her backyard and nibbling on carrots",
+      },
+    ],
+  },
+];
+
+// 1. How many individuals are currently employed?
+const countEmployedIndividuals = function (records) {
+  const employedIndividuls = records.filter(
+    (individual) => individual.currentlyEmployed
+  );
+
+  return employedIndividuls.length;
+};
+
+console.log(countEmployedIndividuals(peopleDetails));
